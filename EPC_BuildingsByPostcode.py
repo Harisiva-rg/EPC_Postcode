ad9b1e398dc83d276ea791963b0ee4dbf5a4dadf
Harisiva-rg <120852576+Harisiva-rg@users.noreply.github.com>
2023-07-25 18:03:20 +0100

    Update EPC_BuildingsByPostcode.py
    Simplified the search and added new filter

--- EPC_BuildingsByPostcode.py
+++ EPC_BuildingsByPostcode.py
@@ -1,77 +1,63 @@
+import pandas as pd
 import dash
 from dash import html, dash_table, dcc
-import pandas as pd
 from dash.dependencies import Input, Output, State
 from datasets import load_dataset
 
 dataset = load_dataset('HarisivaRG/EPC_Postcode_grouped')
-EPC_postcode_group = pd.DataFrame(dataset['train'])
+EPC_postcode_group = dataset['train'].to_pandas()
 
 EPC_top_100 = EPC_postcode_group.head(100)
-# EPC_top_100.loc[:, 'Serial No'] = range(1, 101)
+
 
 app = dash.Dash(__name__)
 server = app.server
 app.title = "Domestic Buildings Based on Postcodes"
 
 app.layout = html.Div([
-    html.H2("Top 100 postcodes based on building counts", id="subtitle"),
-    html.Label("Filter on"),
+    html.H2("Top postcodes based on building counts", id="subtitle"),
+    html.Label("Built Form"),
     dcc.Dropdown(
         id='built-form-dropdown',
         options=[{'label': i, 'value': i} for i in EPC_postcode_group['BUILT_FORM'].unique()],
         value=EPC_postcode_group['BUILT_FORM'].unique()[0],
         multi=True
     ),
-    # dcc.Checklist(
-    #     id='built-form-checkbox',
-    #     options=[{'label': i, 'value': i} for i in EPC_postcode_group['BUILT_FORM'].unique()],
-    #     value=[EPC_postcode_group['BUILT_FORM'].unique()[0]]
-    # ),
+    html.Label("Property type"),
+    dcc.Dropdown(
+        id='property-type-dropdown',
+        options=[{'label': i, 'value': i} for i in EPC_postcode_group['PROPERTY_TYPE'].unique()],
+        value=EPC_postcode_group['PROPERTY_TYPE'].unique(),
+        multi=True
+    ),
     dash_table.DataTable(
         id='table',
-        columns=[{"name": i, "id": i} for i in EPC_top_100.columns],
-        data=EPC_top_100.to_dict('records'), 
-        page_size=50  # Set the page size
-    ),
-    html.Button('Load More', id='load-more-button', n_clicks=0),  # Add a button component
-    html.Div(id='page-number', style={'display': 'none'})  # Hidden div to store the current page number
+        columns=[{"name": "Serial No", "id": "Serial No"}] + [{"name": i, "id": i} for i in EPC_top_100.columns],
+        data=[],
+    )
 ])
 
 @app.callback(
     Output('table', 'data'),
     Output('subtitle', 'children'),
     Input('built-form-dropdown', 'value'),
-    State('table', 'page_current'),  # Get the current page number
-    State('page-number', 'children')  # Get the previous page number
+    Input('property-type-dropdown', 'value')
 )
 
-def update_table(built_forms, current_page_number, prev_page_number):
+def update_table(built_forms, property_types):
     if isinstance(built_forms, str):
         built_forms = [built_forms]
+    if isinstance(property_types, str):
+        property_types = [property_types]
     filtered_df = EPC_postcode_group[EPC_postcode_group['BUILT_FORM'].isin(built_forms)]
-    page_size = 50  # Set the page size
-    if prev_page_number is None:
-        prev_page_number = 0
-    current_page_number = int(prev_page_number) + 1  # Increment the current page number
-    start_index = current_page_number * page_size
-    end_index = start_index + page_size
-    page_df = filtered_df.iloc[:end_index]  # Slice the DataFrame to return the current page
-    page_df = page_df.iloc[start_index:end_index]  # Slice the DataFrame to return the next page
-    page_df['Serial No'] = range(start_index + 1, end_index + 1)  # Add serial number column
-    subtitle = f"Top {len(page_df)} postcodes with {', '.join(built_forms)} buildings"
-    return page_df.to_dict('records'), subtitle
-
-@app.callback(
-    Output('page-number', 'children'),
-    Input('load-more-button', 'n_clicks'),
-    State('page-number', 'children')
-)
-def update_page_number(n_clicks, prev_page_number):
-    if prev_page_number is None:
-        prev_page_number = 0
-    current_page_number = int(prev_page_number) + 1  # Increment the current page number
-    return current_page_number
+    filtered_df = filtered_df[filtered_df['PROPERTY_TYPE'].isin(property_types)]
+    filtered_df = filtered_df.sort_values(by='count', ascending=False)
+    top_100_df = pd.concat([filtered_df[filtered_df['BUILT_FORM'] == bf].head(100) for bf in built_forms])
+    top_100_df= top_100_df.sort_values(by='count', ascending=False)
+    top_100_df['Serial No'] = range(1, len(top_100_df)+1)
+    top_100_df = top_100_df[['Serial No'] + list(top_100_df.columns[:-1])]
+    subtitle = f"Top postcodes with {', '.join(built_forms)} buildings"
+    return top_100_df.to_dict('records'), subtitle
 
 if __name__ == '__main__':
     app.run_server(debug=True)
